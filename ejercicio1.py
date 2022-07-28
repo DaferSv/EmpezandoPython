51a6f989ca422340b8dddbb3d9c8ce98bd0ac04a
daf3r <isaacdavid959@gmail.com>
2022-07-28 14:21:31 -0500

    Ejercicios practicos de la semana 1

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+#Ejercicio donde se le pide al usuario el codigo de area y numero de telefono
+print("===================================")
+print("Codigo de area y numero de telefono")
+print("===================================")
+
+#Empieza el programa (procesos)
+CodArea = input("Codigo de area: ")
+print("===================================")
+NumTel = input("Digite el numero de telefono: ")
+print("===================================")
+print ("Su numero de telefono: "+NumTel)
---
+++ ejercicio4.py
@@ -0,0 +1,3 @@
+#Escribir todo el nombre en una variable y mostrar solo el apellido
+
+name=input("Digite un nombre y un apellido: ")
