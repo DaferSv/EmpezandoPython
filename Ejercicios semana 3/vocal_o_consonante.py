b2cce4ec51638e5cdd9cc578682df89e2e6ec3b3
Dafer <87869353+DaferSv@users.noreply.github.com>
2022-08-21 19:10:27 -0600

    Update Exercise A
    ---

---
+++ Ejercicios semana 3/vocal_o_consonante.py
@@ -0,0 +1,22 @@
+
+
+
+def vocal_o_conosonante(letra):
+        
+    if letra in ('a','e','i','o','u','A','E','I','O','U'):
+        print ("esta es una vocal")
+    elif letra in ("y","Y"):
+        print ("esta es una vocal u consonante")
+    else:
+        print ("esta es una constante")
+
+l=input("ingrese una letra cualquiera:")
+
+print=(vocal_o_conosonante(l))
+
+
+
+
+
+
+
